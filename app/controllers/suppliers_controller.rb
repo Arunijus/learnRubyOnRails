1a26f4147dd4ad92b6fc08763546a872f31a96ea
Arunas <arunijus@gmail.com>
2017-03-24 21:35:54 +0200

    supplier show action and view added

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -2,4 +2,9 @@ class SuppliersController < ApplicationController
   def index
     @suppliers = Supplier.all
   end
+
+  def show
+    @supplier = Supplier.find(params[:id])
+    @supplier_items = @supplier.supplier_items
+  end
 end
