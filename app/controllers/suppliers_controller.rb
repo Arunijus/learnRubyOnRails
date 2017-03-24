407af0e6fd6f9f3fda149e27cfe5a8029736a73e
Arunas <arunijus@gmail.com>
2017-03-24 22:25:19 +0200

    crate new supplier action added

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -7,4 +7,21 @@ class SuppliersController < ApplicationController
     @supplier = Supplier.find(params[:id])
     @supplier_items = @supplier.supplier_items
   end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    if Supplier.create(supplier_params)
+      redirect_to(:action => 'index')
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def supplier_params
+      params.require(:supplier).permit(:name, :address)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/suppliers' => 'suppliers#index'
-  get '/suppliers/:id' => 'suppliers#show', as: :supplier
+  get '/suppliers/show/:id' => 'suppliers#show', as: :supplier
+  get '/suppliers/new' => 'suppliers#new', as: :new_supplier
+
+  post '/suppliers' => 'suppliers#create'
 end
